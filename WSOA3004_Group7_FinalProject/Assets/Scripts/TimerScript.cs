efa103f1d1c18a05de15c15e0f407a1f413bf4e3
vividcurlyfry <amy.pegram@hotmail.com>
2020-08-30 16:30:04 +0200

    Added UI

---
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float Seconds = 0;
+    public Image DayImage;
+    public float MinutesInDay = 20;
+    public float SecondsInDay;
+    public float SecondTimer;
+    public float OneSecTimer = 0;
+
+    private void Start()
+    {
+        SecondsInDay = MinutesInDay * 60;
+        SecondTimer = 1 / SecondsInDay;
+    }
+
+    void Update()
+    {
+        Seconds += Time.deltaTime;
+        OneSecTimer += Time.deltaTime;
+        if(OneSecTimer >= 1)
+        {
+            DayImage.fillAmount = DayImage.fillAmount - SecondTimer;
+            OneSecTimer = 0;
+        }
+    }
+}
