b9d997b0c9118d32958718755ed40b8b4b478b6c
vividcurlyfry <amy.pegram@hotmail.com>
2020-09-07 13:05:44 +0200

    Implementing UI

---
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/GridScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridScript : MonoBehaviour
+{
+    public GameObject square;
+    public Tilemap tm_highlight;
+    public Tile highlight;
+    public List<Vector3Int> HighlightedTiles;
+    private void OnMouseOver()
+    {
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int selectedTile = tm_highlight.WorldToCell(point);
+        if (!HighlightedTiles.Contains(selectedTile))
+        {
+            tm_highlight.SetTile(selectedTile, highlight);
+            HighlightedTiles.Add(selectedTile); 
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        for(int i = 0; i < HighlightedTiles.Count; i++)
+        {
+            tm_highlight.SetTile(HighlightedTiles[0], null);
+        }
+    }
+    private void Update()
+    {
+        if(HighlightedTiles.Count > 1)
+        {
+           tm_highlight.SetTile(HighlightedTiles[0], null);
+           HighlightedTiles.RemoveAt(0);
+        }
+    }
+}
