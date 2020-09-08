4e050b2ae5d1c84ff9bdb071befcf35f5ee61899
r4inyb0w <1827593@students.wits.ac.za>
2020-09-09 00:36:25 +0200

    added basic UI code

---
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/UIScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//control game UI //Samantha Thurgood 1827593
+public class UIScript : MonoBehaviour
+{
+    public GameObject MenuPanel;
+
+    public void OnClickQuests()
+    {
+        //activate quests panel //not made yet
+    }
+
+    public void OnClickShop()
+    {
+        //activate shop panel //not made yet
+    }
+
+    public void OnClickMenu()
+    {
+        if(MenuPanel.activeSelf)
+        {
+            MenuPanel.SetActive(false);
+        }
+        else
+        {
+            MenuPanel.SetActive(true);
+        }
+    }
+
+    public void OnClickMainMenu()
+    {
+        //open main menu screen //not made yet
+    }
+
+    public void OnClickSaveSlots()
+    {
+        //open save slots screen //not made yet
+    }
+
+    //quits game
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
