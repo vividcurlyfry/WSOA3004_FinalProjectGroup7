183db34b235a5eaa2f65843edb6a5656ca7c89be
r4inyb0w <1827593@students.wits.ac.za>
2020-09-21 19:37:35 +0200

    Added animation changing for Ruby - need actual assets now

--- WSOA3004_Group7_FinalProject/Assets/Scripts/NPCScripts/NPCMovement.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/NPCScripts/NPCMovement.cs
@@ -12,6 +12,7 @@ public class NPCMovement : MonoBehaviour
     private float speed = 1;
     private float pause = 0;
     private float setPause = 2;
+    private bool isPaused = false;
 
     private void Start()
     {
@@ -30,12 +31,14 @@ public class NPCMovement : MonoBehaviour
         if (this.transform.position == nextPosition)
         {
             pause += Time.deltaTime;
+            isPaused = true;
         }
 
         if (pause > setPause)
         {
             nextPosition = new Vector3(destination.transform.position.x, destination.transform.position.y, destination.transform.position.z);
             pause = 0;
+            isPaused = false;
         }
 
         if (this.transform.position == home)
@@ -45,6 +48,46 @@ public class NPCMovement : MonoBehaviour
         else
         {
             this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 20;
+        }
+
+        updateAnimation(); //check this placement, might be more dynamic for Rubys movement, might look sus? Could put in if where next is set and if when paused instead??
+    }
+
+    public void updateAnimation()
+    {
+        if (!isPaused)
+        {
+            if((Mathf.Abs(Mathf.Abs(nextPosition.y) - Mathf.Abs(this.transform.position.y))) > (Mathf.Abs(Mathf.Abs(nextPosition.x) - Mathf.Abs(this.transform.position.x))))
+            {
+                if (nextPosition.y < this.transform.position.y)
+                {
+                    //trigger face up animation
+                    print("face forward");
+                }
+                else if (nextPosition.y > this.transform.position.y)
+                {
+                    //trigger back animation
+                    print("backward");
+                }
+            }
+            else
+            {
+                if (nextPosition.x < this.transform.position.x)
+                {
+                    //trigger left animation
+                    print("left");
+                }
+                else if (nextPosition.x > this.transform.position.x)
+                {
+                    //trigger right animation
+                    print("right");
+                }
+            }
+        }
+        else
+        {
+            //idle animation
+            print("idle");
         }
     }
 }
