52f5ca920c3709f54be31edb268db3c6730e27df
r4inyb0w <1827593@students.wits.ac.za>
2020-10-01 15:03:03 +0200

    fixed some liveliness and speech bubble placement

--- WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/LivelinessEffects.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/LivelinessEffects.cs
@@ -17,7 +17,7 @@ public class LivelinessEffects : MonoBehaviour
     //maybe playtest chance of rain
     public void LivelinessSet()
     {
-        randomNum = Random.Range(0,4);
+        randomNum = Random.Range(0,3);
 
         switch (randomNum)
         {
--- WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/NPCScripts/DialogueTrigger.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/NPCScripts/DialogueTrigger.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public DialogueObject Day1, Day2, Day3;
-    private int today = 3;
+    public DialogueObject Day1, Day2, Day3, RainDay;
+    private int today = 1;
+    private bool isRaining = false;
 
     //on ruby click first time
     public void TriggerDialogue()
     {
-        //add rainy day
-        //today = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>().Today; //Amy needs to add this connection
-        if (today == 1)
-        {
-            FindObjectOfType<Dialogue>().StartDialogue(Day1);
-        }
-        else if (today == 2)
-        {
-            FindObjectOfType<Dialogue>().StartDialogue(Day2);
-        }
-        else if (today == 3)
-        {
-            FindObjectOfType<Dialogue>().StartDialogue(Day3);
-        }
+        isRaining = GameObject.FindGameObjectWithTag("GameManager").GetComponent<LivelinessEffects>().Raining;
+        //today = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>().Today;
 
+        if(isRaining)
+        {
+            FindObjectOfType<Dialogue>().StartDialogue(RainDay);
+        }
+        else
+        {
+            if (today == 1)
+            {
+                FindObjectOfType<Dialogue>().StartDialogue(Day1);
+            }
+            else if (today == 2)
+            {
+                FindObjectOfType<Dialogue>().StartDialogue(Day2);
+            }
+            else if (today == 3)
+            {
+                FindObjectOfType<Dialogue>().StartDialogue(Day3);
+            }
+        }
     }
 }
