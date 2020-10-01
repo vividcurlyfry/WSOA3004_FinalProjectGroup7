5bea0194b5bc879cd921fc18a86664d8e7ec6884
r4inyb0w <1827593@students.wits.ac.za>
2020-10-01 14:29:57 +0200

    Added LivelinessEffects

---
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/LivelinessEffects.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivelinessEffects : MonoBehaviour
+{
+    public bool Raining = false;
+    public GameObject RainEffect, BeesEffect;
+
+    private int randomNum = 0;
+
+    private void Start()
+    {
+        LivelinessSet();
+    }
+
+    //maybe playtest chance of rain
+    public void LivelinessSet()
+    {
+        randomNum = Random.Range(0,4);
+
+        switch (randomNum)
+        {
+            case 0:
+                RainEffect.SetActive(true);
+                BeesEffect.SetActive(false);
+                Raining = true;
+                break;
+            default:
+                RainEffect.SetActive(false);
+                BeesEffect.SetActive(true);
+                Raining = false;
+                break;
+        }
+    }
+}
