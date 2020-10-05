c86865292f01f94cbbe3ca146da4eb54f12ead60
r4inyb0w <1827593@students.wits.ac.za>
2020-10-05 17:19:49 +0200

    add temp dayover scene

---
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/TruckMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckMovement : MonoBehaviour
+{
+    private Vector3 endPosition, startPosition;
+    public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = new Vector3(-16, -0.75f, 1);
+        endPosition = new Vector3(16, -0.75f, 1);
+        this.transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, endPosition, speed * Time.deltaTime);
+    }
+}
