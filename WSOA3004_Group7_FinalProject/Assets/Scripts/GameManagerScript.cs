8a4f7153310259ec736de0d6c0d1ac66b06cb85a
vividcurlyfry <amy.pegram@hotmail.com>
2020-10-08 00:04:56 +0200

    started delivery stuff

--- WSOA3004_Group7_FinalProject/Assets/Scripts/GameManagerScript.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/GameManagerScript.cs
@@ -19,6 +19,8 @@ public class GameManagerScript : MonoBehaviour
     public Text FundsText;
     public bool isRaining;
 
+    public Order order1;
+
     public Crop LettuceSeed;
     public Crop PotatoSeed;
     public Crop TurnipSeed;
---
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Order", menuName = "Orders")]
+[System.Serializable]
+public class Order : ScriptableObject
+{
+    public string OrderText;
+    public int TurnipAmount;
+    public int PeachAmount;
+    public int WatermelonAmount;
+    public int CarrotAmount;
+    public int PotatoAmount;
+    public int LettuceAmount;
+}
