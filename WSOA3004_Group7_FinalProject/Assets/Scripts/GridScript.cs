c649800bc639754f0bcf11aa9f54bb14a6798d4f
vividcurlyfry <amy.pegram@hotmail.com>
2020-10-09 21:28:38 +0200

    build!

--- WSOA3004_Group7_FinalProject/Assets/Scripts/GridScript.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/GridScript.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro.EditorUtilities;
+//using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
