3345ba8aaf5680bd2689110ad8c94d6f57bc5e2b
Tim Flusk <tpflusk@gmail.com>
2020-10-21 16:30:42 +0200

    Potential fix for animation movement

--- WSOA3004_Group7_FinalProject/Assets/Scripts/MovementScript.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/MovementScript.cs
@@ -20,6 +20,81 @@ public class MovementScript : MonoBehaviour
     {
         motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        //OriginalAnimationWrapper();
+        
+        NewAnimationWrapper();
+        
+    }
+    
+    void FixedUpdate()
+    {
+        Move();
+        
+    }
+
+    private void NewAnimationWrapper()
+    {
+        
+        /////////////////////////////////////////////////
+        /// Walking Forwards
+        
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("FrontWalk");
+            anim.ResetTrigger("Idle");
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetTrigger("Idle");
+            anim.ResetTrigger("FrontWalk");
+        }
+
+        /////////////////////////////////////////////////
+        /// Walking Backwards
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("BackWalk");
+            anim.ResetTrigger("Idle1");
+        }
+            
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetTrigger("Idle1");
+            anim.ResetTrigger("BackWalk");
+        }
+        
+        /////////////////////////////////////////////////
+        /// Left Walk
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("LeftWalk");
+            anim.ResetTrigger("Idle2");
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetTrigger("Idle2");
+            anim.ResetTrigger("LeftWalk");
+        }
+        
+        
+        /////////////////////////////////////////////////
+        /// Right Walk
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("RightWalk");
+            anim.ResetTrigger("Idle3");
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetTrigger("Idle3");
+            anim.ResetTrigger("RightWalk");
+        }
+    }
+    
+    private void OriginalAnimationWrapper()
+    {
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
             anim.SetTrigger("FrontWalk");
         if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
@@ -41,10 +116,12 @@ public class MovementScript : MonoBehaviour
             anim.SetTrigger("Idle3");
     }
 
-    void FixedUpdate()
+    public void ResetTrigger(string triggerName)
     {
-        Move();
+        anim.ResetTrigger(triggerName);
     }
+
+    
 
     private void Move()
     {
