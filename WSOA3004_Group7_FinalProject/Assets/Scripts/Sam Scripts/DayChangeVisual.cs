40ed78f72c909b7988ced7c8ff9a2277cc270958
r4inyb0w <1827593@students.wits.ac.za>
2020-10-22 14:31:50 +0200

    update: ruby heart fill. transition, speech bubbles

--- WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/DayChangeVisual.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/DayChangeVisual.cs
@@ -12,8 +12,9 @@ public class DayChangeVisual : MonoBehaviour
     public Sprite LightOn, LightOff;
     public AudioSource AS;
     public AudioClip nightAudio;
+    public GameObject Ruby;
 
-    private float t = 0, speed = 1f, timeBetween = 0;
+    private float t1 = 0, t2 = 0, speed = 1f, timeBetween = 0, timeStart = 0;
     private int today = 0;
 
     // Start is called before the first frame update
@@ -21,25 +22,45 @@ public class DayChangeVisual : MonoBehaviour
     {
         AS.volume = 0;
         dayChange = false;
-        today = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>().DaysPlayed + 2;
+        today = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>().DaysPlayed + 1;
         dayOver = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>().dayOver;
+        GameObject.FindGameObjectWithTag("NPC").GetComponent<NPCMovement>().enabled = true;
         dayChange = dayOver;
+
+        t1 = 0;
+        t2 = 2;
+        timeStart = 0;
+        timeBetween = 0;
+
+        this.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 0f);
+        date.text = today.ToString();
+        Calendar.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        t = speed * Time.deltaTime;
         dayOver = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>().dayOver;
         dayChange = dayOver;
 
+        //start of day
+        timeStart += Time.deltaTime;
+        if(timeStart > 2f)
+        {
+            t1 = speed * Time.deltaTime;
+            Calendar.SetActive(false);
+        }
+
+        //end of day
         if (dayChange)
         {
+            GameObject.FindGameObjectWithTag("NPC").GetComponent<NPCMovement>().enabled = false;
+            t2 = 10f * speed * Time.deltaTime;
             AS.volume = Mathf.Lerp(0, 0.5f, 0.9f);
             Lamp.gameObject.GetComponent<SpriteRenderer>().sprite = LightOn;
             Lamp.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 202;
 
-            this.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, Mathf.Lerp(this.GetComponent<SpriteRenderer>().color.a, 1, t));
+            this.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, Mathf.Lerp(this.GetComponent<SpriteRenderer>().color.a, 1, t2));
             timeBetween += Time.deltaTime;
         }
 
@@ -52,23 +73,14 @@ public class DayChangeVisual : MonoBehaviour
         if (timeBetween > 6f) // test time length
         {
             Lamp.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 8;
+            dayChange = false;
+         
         }
 
         if (timeBetween > 7f) // test time length
         {
-            date.text = today.ToString();
-            Calendar.SetActive(true);
-        }
-
-        if (timeBetween > 9f) // test time length
-        {
-            Calendar.SetActive(false);
-        }
-
-        if (timeBetween > 10f) // test time length
-        {
-            dayChange = false;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
     }
 }
--- WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/NPCScripts/NPCMouseClick.cs
+++ WSOA3004_Group7_FinalProject/Assets/Scripts/Sam Scripts/NPCScripts/NPCMouseClick.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCMouseClick : MonoBehaviour
 {
     private int clicks = 0;
     private float startTime, endTime;
-    private bool next = true;
+    private bool next = true, clicked = false;
 
     public GameObject ShopUI;
-    public GameObject Bed;
+    public GameObject Bed, heart;
+    public Image fillHeart;
 
     private void Update()
     {
-        if (this.transform.position == new Vector3(17.69f, 15, -1))
+        if (((this.transform.position.x > 14.9f) && (this.transform.position.x < 17.1f)) && ((this.transform.position.x > 14.9f)&&(this.transform.position.y < 20.1f))) //check this??
         {
             Bed.SetActive(false);
         }
@@ -21,17 +23,37 @@ public class NPCMouseClick : MonoBehaviour
         {
             Bed.SetActive(true);
         }
-    }
 
+        
+        if(clicked)
+        {
+            fillHeart.fillAmount += 0.03f;
+        }
+
+
+    }
+    
     private void OnMouseDown()
     {
         startTime = Time.time;
+        if (!GameObject.FindGameObjectWithTag("NPC").GetComponent<NPCMovement>().talking)
+        {
+            clicked = true;
+            heart.SetActive(true);
+        }
+        else
+        {
+            clicked = false;
+        }
     }
 
     private void OnMouseUp()
     {
         endTime = Time.time;
         next = GameObject.FindGameObjectWithTag("Dialogue").GetComponent<Dialogue>().next;
+        clicked = false;
+        heart.SetActive(false);
+        fillHeart.fillAmount = 0;
 
         //checking if in the house or shop UI is up
         if ((this.transform.position == new Vector3(17.69f, 16f, -1)) || (ShopUI.gameObject.activeSelf))
